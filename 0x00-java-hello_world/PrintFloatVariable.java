6589a8a8fe8a9f8a0cb7895dda68c2d010fda5fe
mdelagracia <mdelagracia@ciandt.com>
2022-06-14 13:21:41 -0300

    Task 2

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -3,4 +3,4 @@ public class PrintFloatVariable {
     public static void main(String[] args) {
         Locale.setDefault(new Locale("en", "US"));
         float pi = (float) 3.14159;
-        System.out.printf("Valor de pi: %.3f %n", pi);}}
+        System.out.printf("Valor de pi: %.3f%n",pi);}}
