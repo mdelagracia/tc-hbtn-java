f87e89622f769a8f0c0edb2b1168a45955621969
mdelagracia <mdelagracia@ciandt.com>
2022-06-14 10:11:07 -0300

    Task 2

---
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -0,0 +1,6 @@
+public class PrintFloatVariable{
+    public static void main(String[] args) {
+        float pi = (float) 3.14159265359;
+        System.out.printf("Valor de pi: ");
+        System.out.printf("%.3f",pi);    }
+}
