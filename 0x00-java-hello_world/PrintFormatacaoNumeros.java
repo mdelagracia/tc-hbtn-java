42ba4d9547fe12fc143c8cb15f9012a1595d997f
mdelagracia <mdelagracia@ciandt.com>
2022-06-14 17:50:46 -0300

    Task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -4,8 +4,12 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale.setDefault(newLocale("en","US"));
-        System.out.printf("Valor: R$ %.2f%n", valor);
-        System.out.printf("Taxa: %.2f%%%n", taxa);
-    }
+
+        Locale localeBR = new Locale("en","US");
+        NumberFormat valorFormatado = NumberFormat.getInstance(localeBR);
+        valorFormatado.setMaximumFractionDigits(2);
+        System.out.println("Valor: R$ " +  valorFormatado.format(valor));
+        System.out.printf(Locale.US,"Taxa: %.2f%%%n", taxa);
+        }
 }
+
