8659deae60f2d6241123baef783ebcb62809b609
mdelagracia <mdelagracia@ciandt.com>
2022-06-19 22:31:23 -0300

    Task 3

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,42 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int tomarDano(int quantidadeDeDano) {
+
+        int saude = getSaudeAtual()- quantidadeDeDano;
+        setSaudeAtual(saude);
+        return getSaudeAtual();
+    }
+
+    public int receberCura(int quantidadeDeCura) {
+        int saude = getSaudeAtual() + quantidadeDeCura;
+        setSaudeAtual(saude);
+        return getSaudeAtual();
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if (saudeAtual <= 0) { saudeAtual = 0; status = "morto";}
+        if (saudeAtual > 0) { status = "vivo";}
+        if (saudeAtual > 100) {saudeAtual = 100;}
+        this.saudeAtual = saudeAtual;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
