fc1e8b5c17495e173a866d8f880fa393a4ffdbdc
mdelagracia <mdelagracia@ciandt.com>
2022-06-21 18:08:24 -0300

    Task 2

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,5 +1,5 @@
 public class Caractere {
-    public static boolean EhMaiusculo(int c){
+    public static boolean EhMaiusculo(char c){
         if( c >= 'a' && c <= 'z' )
         {return false;}
         else
