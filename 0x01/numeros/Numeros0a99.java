71dd4d9366a5b7481d108c861056974ce296a90a
mdelagracia <mdelagracia@ciandt.com>
2022-06-21 17:23:01 -0300

    Task 0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,13 @@
+public class Numeros0a99 {
+    public int numero;
+
+    public static void main(String[] args) {
+        for (int numero = 0; numero <= 99; numero++) {
+            if (numero < 99) {
+                System.out.print(numero + ", ");
+            } else {
+                System.out.print(numero);
+            }
+        }
+    }
+}
