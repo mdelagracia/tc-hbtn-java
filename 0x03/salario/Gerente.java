9cb7ab7a426fdc54ff92caeed3e9914f2191ed39
mdelagracia <mdelagracia@ciandt.com>
2022-06-24 18:13:07 -0300

    Task 0

--- 0x03/salario/Gerente.java
+++ 0x03/salario/Gerente.java
@@ -1,12 +1,14 @@
-public class Gerente extends Empregado{
+public class Gerente extends Empregado {
 
     public Gerente(double salarioFixo) {
         super(salarioFixo);
     }
+
     @Override
-    public double calcularBonus(Departamento departamento){
-        if (departamento.alcancouMeta() == true){
-        bonus =  salarioFixo * 0.2 + (departamento.valorAtingidoMeta%departamento.valorMeta * 0.01);}
+    public double calcularBonus(Departamento departamento) {
+        if (departamento.alcancouMeta() == true) {
+            bonus = salarioFixo * 0.2 + (departamento.valorAtingidoMeta % departamento.valorMeta * 0.01);
+        }
         return bonus;
 
     }
