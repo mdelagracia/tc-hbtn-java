b2535c19c9a308add61ff4bf0b81d17fdcc5596c
mdelagracia <mdelagracia@ciandt.com>
2022-06-26 11:28:26 -0300

    Task 1

--- 0x03/livraria/Program.java
+++ 0x03/livraria/Program.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt","BR"));
 
         try {
             Livro livro1 = new Livro(
