e5e253fbb59e727517ce9bdbfda97fae8c270508
mdelagracia <mdelagracia@ciandt.com>
2022-07-10 21:04:40 -0300

    Task 0

---
+++ Generics/midias/Livro.java
@@ -0,0 +1,16 @@
+public class Livro extends Midia{
+    private int edicao;
+
+    public Livro(String nome) {
+        super(nome);
+    }
+
+    public Livro(String nome, int edicao) {
+        super(nome);
+        this.edicao = edicao;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+}
