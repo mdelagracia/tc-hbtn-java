a80345c62dfb6997dc1f4d8ff8a5e7c5696c1dd0
mdelagracia <mdelagracia@ciandt.com>
2022-07-12 21:38:12 -0300

    Task 3

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Pedido.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private int codigo;
+    List<Produto> produtos;
+    private Cliente cliente;
+
+    @Override
+    public String toString() {
+        return "["+codigo+"] "+cliente.getNome();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Pedido(int codigo, List<Produto> produtos, Cliente cliente) {
+        this.codigo = codigo;
+        this.produtos = produtos;
+        this.cliente = cliente;
+    }
+}
